7a06fb6a63a667b103e69dc32c7195c11fb1e97a
robert-95 <robert.schleinhege@ionos.com>
2021-05-01 17:21:33 +0200

    all files

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  purge: [],
+  theme: {
+    screens: {
+      sm: '640px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1440px',
+    },
+    extend: {
+      boxShadow: {
+        rainbow: '0 0 0 10px #ff0000,0 0 0 20px #ff7700,0 0 0 30px #FFDD00,0 0 0 40px #00FF00,0 0 0 50px #0000FF,0 0 0 60px #C77DF3,0 0 0 70px #8A2BE2',
+      },
+      fontFamily: {
+        serif: ['Pacifico', 'sans-serif'],
+        sans: ['Dosis', 'sans-serif'],
+      },
+      colors: {
+        blue: {
+          '100': '#72c1df',
+          '200': '#5bb7d9',
+          '300': '#43add4',
+          '400': '#2ca2ce',
+          '500': '#1498c9',
+          '600': '#1289b5',
+          '700': '#107aa1',
+          '800': '#0e6a8d',
+          '900': '#0c5b79'
+        },
+        gold: {
+          light: '#ddbf5f',
+          base: '#d4af37',
+          dark: '#aa8c2c'
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
