0af71b525bc6d57451318475ffc1e6dcfdb2651a
robert-95 <robert.schleinhege@ionos.com>
2021-05-03 21:04:59 +0200

    breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   purge: [],
   theme: {
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1.5rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
     screens: {
       'sm': '640px',
       // => @media (min-width: 640px) { ... }
@@ -17,40 +31,40 @@ module.exports = {
       '2xl': '1736px',
       // => @media (min-width: 1536px) { ... }
     },
-      extend: {
-        boxShadow: {
-          rainbow: '0 0 0 10px #ff0000,0 0 0 20px #ff7700,0 0 0 30px #FFDD00,0 0 0 40px #00FF00,0 0 0 50px #0000FF,0 0 0 60px #C77DF3,0 0 0 70px #8A2BE2',
-        },
-        fontFamily: {
-          serif: ['Pacifico', 'sans-serif'],
-          sans: ['Dosis', 'sans-serif'],
-        },
-        colors: {
-          myred: {
-            base: "#E53835",
-            light: "#f5706e",
-            dark: "#990200"
-
-          },
-          blue: {
-            '100': '#72c1df',
-            '200': '#5bb7d9',
-            '300': '#43add4',
-            '400': '#2ca2ce',
-            '500': '#1498c9',
-            '600': '#1289b5',
-            '700': '#107aa1',
-            '800': '#0e6a8d',
-            '900': '#0c5b79'
-          },
-          gold: {
-            light: '#ddbf5f',
-            base: '#d4af37',
-            dark: '#aa8c2c'
-          }
-        }
+    extend: {
+      boxShadow: {
+        rainbow: '0 0 0 10px #ff0000,0 0 0 20px #ff7700,0 0 0 30px #FFDD00,0 0 0 40px #00FF00,0 0 0 50px #0000FF,0 0 0 60px #C77DF3,0 0 0 70px #8A2BE2',
       },
+      fontFamily: {
+        serif: ['Pacifico', 'sans-serif'],
+        sans: ['Dosis', 'sans-serif'],
+      },
+      colors: {
+        myred: {
+          base: "#E53835",
+          light: "#f5706e",
+          dark: "#990200"
+
+        },
+        blue: {
+          '100': '#72c1df',
+          '200': '#5bb7d9',
+          '300': '#43add4',
+          '400': '#2ca2ce',
+          '500': '#1498c9',
+          '600': '#1289b5',
+          '700': '#107aa1',
+          '800': '#0e6a8d',
+          '900': '#0c5b79'
+        },
+        gold: {
+          light: '#ddbf5f',
+          base: '#d4af37',
+          dark: '#aa8c2c'
+        }
+      }
     },
-    variants: {},
-    plugins: [],
-  }
+  },
+  variants: {},
+  plugins: [],
+}
